63a29d042356c610dbf62ce3d4e225344eb0f74c
akarshita <akarshita253@gmail.com>
2025-12-05 13:18:20 +0530

    debugging error -1

--- app/(dynamic_pages)/blogs/page.tsx
+++ app/(dynamic_pages)/blogs/page.tsx
@@ -1,34 +1,46 @@
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-import { Cards } from "@/components/shared/Cards";
-import { AllBlogsQuery } from "@/features/blog/types/blogTypes";
-import { GET_BLOGS } from "@/lib/queries/getBlogs";
-import { strapiRequest } from "@/lib/utils";
-import Link from "next/link";
+// import { Cards } from "@/components/shared/Cards";
+// import { AllBlogsQuery } from "@/features/blog/types/blogTypes";
+// import { GET_BLOGS } from "@/lib/queries/getBlogs";
+// import { strapiRequest } from "@/lib/utils";
+// import Link from "next/link";
 
-const BlogList = async () => {
-  const data = await strapiRequest<AllBlogsQuery>(GET_BLOGS);
-  const allBlogs = data?.blogs;
+// const BlogList = async () => {
+//   const data = await strapiRequest<AllBlogsQuery>(GET_BLOGS);
+//   const allBlogs = data?.blogs;
+//   return (
+//     <main>
+//       {allBlogs?.map((blog) => {
+//         const imageUrl = {
+//           url: blog.featureImage?.url,
+//           alternativeText: blog.featureImage?.alternativeText,
+//         };
+//         return (
+//           <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
+//             <Cards
+//               title={blog.title || ""}
+//               createdAt={blog.createdAt}
+//               description={""}
+//               imageUrl={imageUrl}
+//             />
+//           </Link>
+//         );
+//       })}
+//     </main>
+//   );
+// };
+
+// export default BlogList;
+import React from 'react'
+
+const BlogList = () => {
   return (
-    <main>
-      {allBlogs?.map((blog) => {
-        const imageUrl = {
-          url: blog.featureImage?.url,
-          alternativeText: blog.featureImage?.alternativeText,
-        };
-        return (
-          <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
-            <Cards
-              title={blog.title || ""}
-              createdAt={blog.createdAt}
-              description={""}
-              imageUrl={imageUrl}
-            />
-          </Link>
-        );
-      })}
-    </main>
-  );
-};
+    <div>
+      BlogList
+    </div>
+  )
+}
 
-export default BlogList;
+export default BlogList
+BlogList
