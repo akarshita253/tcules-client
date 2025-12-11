ad9b08ee2bdf0a0456c269500d7ca0fd020033ee
akarshita <akarshita253@gmail.com>
2025-12-11 13:57:41 +0530

    added bullet points and numbers in the list

--- components/shared/DisplayContent.tsx
+++ components/shared/DisplayContent.tsx
@@ -3,28 +3,40 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
-import rehypeSanitize from 'rehype-sanitize';
 
 export default function DisplayContent({ content }: { content?: string | null }) {
   if (!content) return null;
 
+  
   return (
-    <div className="">
+    <div>
       <article className="prose prose-lg max-w-none mx-auto py-4">
         <ReactMarkdown
           remarkPlugins={[remarkGfm]}
-          rehypePlugins={[
-            rehypeRaw,          
-            rehypeSanitize 
-          ]}
+          rehypePlugins={[rehypeRaw]}
           components={{
-           
-            h2: ({ children }) => <h2 className="text-heading-xs mb-6">{children}</h2>,
-            p: ({ children }) => <p className="text-label-md mb-2 text-neutral-700">{children}</p>,
-            a: ({ href, children }) => (
-              <a href={href} className="text-blue-600 hover:underline" target="_blank" rel="noopener">
+            h2: ({ children }) => (
+              <h2 className="text-heading-xs mb-4">{children}</h2>
+            ),
+
+            p: ({ children }) => (
+              <p className="text-label-md mb-2 text-neutral-700">{children}</p>
+            ),
+
+            ul: ({ children }) => (
+              <ul className="list-disc mb-2 pl-4 text-neutral-700">
                 {children}
-              </a>
+              </ul>
+            ),
+
+            ol: ({ children }) => (
+              <ol className="list-decimal mb-2 pl-4 text-label-md text-neutral-700">
+                {children}
+              </ol>
+            ),
+
+            li: ({ children }) => (
+              <li className="mb-2 text-label-md pl-4">{children}</li>
             ),
           }}
         >
