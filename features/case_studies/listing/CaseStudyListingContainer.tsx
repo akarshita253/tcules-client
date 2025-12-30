d29e49f69fff901ca01a06b1163cfa1305142e01
akarshita <akarshita253@gmail.com>
2025-12-30 18:31:49 +0530

    added suspense boundary

--- features/case_studies/listing/CaseStudyListingContainer.tsx
+++ features/case_studies/listing/CaseStudyListingContainer.tsx
@@ -5,6 +5,7 @@ import CaseStudyList from "./components/CaseStudyList";
 import ApolloWrapper from "@/lib/providers/ApolloWrapper";
 import { CaseStudyLandingPageQuery } from "@/lib/codegen/graphql";
 import Miscellaneous from '@/components/shared/Miscellaneous'
+import { Suspense } from "react";
 
 const CaseStudyListingContainer = ({caseStudyLandingPageData}: {caseStudyLandingPageData: NonNullable<CaseStudyLandingPageQuery["caseStudyLandingPage"]>}) => {
   const heroSectionData = caseStudyLandingPageData.heroSection;
@@ -13,8 +14,10 @@ const CaseStudyListingContainer = ({caseStudyLandingPageData}: {caseStudyLanding
     <Container className="md:grid grid-cols-12 gap-6">
       <Block className="lg:col-start-2 col-start-1 lg:col-end-12 col-end-13">
         <CaseStudyListingHeroSec heroSectionData={heroSectionData}/>
-        <ApolloWrapper>
-          <CaseStudyList />
+      <ApolloWrapper>
+          <Suspense fallback={<div>Loading case studies...</div>}>
+            <CaseStudyList />
+          </Suspense>
         </ApolloWrapper>
         <Miscellaneous miscSectionData={miscData}/>
       </Block>
