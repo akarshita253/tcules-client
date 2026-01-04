8a09f82d8e0bc1994ba490ed6efb944b1952787f
akarshita <akarshita253@gmail.com>
2026-01-04 17:18:24 +0530

    added dashed styles to the side

--- features/labs_fil/components/LabsFilFifthSection.tsx
+++ features/labs_fil/components/LabsFilFifthSection.tsx
@@ -2,7 +2,9 @@ import Section from "@/components/global/Section";
 import { buttonVariants } from "@/components/ui/button";
 import { LabsFilLandingPageQuery } from "@/lib/codegen/graphql";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
+import CircleInside from "@/public/CircleInside.svg";
 
 const LabsFilFifthSection = ({
   fifthSectionData,
@@ -19,35 +21,54 @@ const LabsFilFifthSection = ({
       <p className="text-label-md text-neutral-200 mb-6 sm:mb-12">
         {fifthSectionData?.description}
       </p>
-      <div className="flex flex-col gap-4 sm:gap-6 pl-12">
-        {fifthSectionData?.cards &&
-          fifthSectionData?.cards.length > 0 &&
-          fifthSectionData?.cards?.map((singleCard) => {
-            return (
-              <div
-                key={singleCard?.id}
-                className="rounded-xl p-5 bg-neutral-800 group transition-all duration-1000"
-              >
-                <h3 className="text-label-4xl text-neutral-100 mb-3">
-                  {singleCard?.heading}
-                </h3>
-                <p className="text-label-md text-neutral-400 ">
-                  {singleCard?.heading}
-                </p>
-                <div className="overflow-hidden transition-all duration-300 max-h-0 group-hover:max-h-20 group-hover:mt-6">
-                  <Link
-                    href={singleCard?.link?.href || "#"}
-                    className={cn(
-                      buttonVariants({ variant: "default" }),
-                      "opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-                    )}
-                  >
-                    {singleCard?.link?.name || "Apply"}
-                  </Link>
+      <div className="flex items-center gap-6 ">
+        <div className="relative flex flex-col items-center">
+          {/* Dashed line */}
+          <div className="absolute top-0 bottom-0 w-px border-l border-dashed border-green-500/60" />
+
+          {fifthSectionData?.cards &&
+            fifthSectionData?.cards.map((_, index, array) => (
+              <Image
+                src={CircleInside}
+                alt="CircleInside"
+                key={index}
+                className={cn(
+                  "relative z-10",
+                  index !== array.length - 1 && "mb-24"
+                )}
+              />
+            ))}
+        </div>
+        <div className="flex flex-col gap-4 sm:gap-6  grow">
+          {fifthSectionData?.cards &&
+            fifthSectionData?.cards.length > 0 &&
+            fifthSectionData?.cards?.map((singleCard) => {
+              return (
+                <div
+                  key={singleCard?.id}
+                  className="rounded-xl p-5 bg-neutral-800 group transition-all duration-1000"
+                >
+                  <h3 className="text-label-4xl text-neutral-100 mb-3">
+                    {singleCard?.heading}
+                  </h3>
+                  <p className="text-label-md text-neutral-400 ">
+                    {singleCard?.heading}
+                  </p>
+                  <div className="overflow-hidden transition-all duration-300 max-h-0 group-hover:max-h-20 group-hover:mt-6">
+                    <Link
+                      href={singleCard?.link?.href || "#"}
+                      className={cn(
+                        buttonVariants({ variant: "default" }),
+                        "opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                      )}
+                    >
+                      {singleCard?.link?.name || "Apply"}
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
     </Section>
   );
--- features/labs_fil/components/LabsFilSixthSection.tsx
+++ features/labs_fil/components/LabsFilSixthSection.tsx
@@ -1,6 +1,9 @@
 import Section from "@/components/global/Section";
 import { LabsFilLandingPageQuery } from "@/lib/codegen/graphql";
 import LabsMarkdownContent from "./LabsMarkdownContent";
+import { cn } from "@/lib/utils";
+import CircleInside from "@/public/CircleInside.svg";
+import Image from "next/image";
 
 const LabsFilSixthSection = ({
   sixthSectionData,
@@ -17,7 +20,7 @@ const LabsFilSixthSection = ({
       <p className="text-label-md text-neutral-200 mb-6 sm:mb-12">
         {sixthSectionData?.description}
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-2 items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 sm:gap-6 p-2 items-center">
         <div className="bg-neutral-800 rounded-xl p-6 flex flex-col gap-6 sm:gap-12">
           <h3 className="text-neutral-50 text-heading-xs">
             {sixthSectionData?.leftCard?.heading}
@@ -31,17 +34,38 @@ const LabsFilSixthSection = ({
             {sixthSectionData?.leftCard?.bottomText}
           </p>
         </div>
-        <div className="flex flex-col gap-6 p-6 pl-16">
-            {sixthSectionData?.rightCard && sixthSectionData?.rightCard.length > 0 && sixthSectionData?.rightCard.map((singleCard) => (
-                <div key={singleCard?.id}>
-                    <h3 className="text-neutral-100 text-heading-xs">
-                        {singleCard?.heading}
-                    </h3>
-                    <p className="text-label-md text-neutral-400">
-                        {singleCard?.description}
-                    </p>
+        <div className="relative flex items-center gap-6">
+          <div className="relative flex flex-col items-center">
+            {/* Dashed line */}
+            <div className="absolute top-0 bottom-0 w-px border-l border-dashed border-green-500/60" />
+
+            {sixthSectionData?.rightCard &&
+              sixthSectionData?.rightCard.map((_, index, array) => (
+                <Image
+                  src={CircleInside}
+                  alt="CircleInside"
+                  key={index}
+                  className={cn(
+                    "relative z-10",
+                    index !== array.length - 1 && "mb-28"
+                  )}
+                />
+              ))}
+          </div>
+
+          <div className="flex flex-col gap-12">
+            {sixthSectionData?.rightCard &&
+              sixthSectionData?.rightCard.map((singleCard, index) => (
+                <div key={index}>
+                  <h3 className="text-heading-xs font-semibold text-neutral-100">
+                    {singleCard?.heading}
+                  </h3>
+                  <p className="mt-1 text-label-md text-neutral-400">
+                    {singleCard?.description}
+                  </p>
                 </div>
-            ))}
+              ))}
+          </div>
         </div>
       </div>
     </Section>
