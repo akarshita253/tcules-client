dd5a0ef129470d70c655aad26424c34557961aaa
akarshita <akarshita253@gmail.com>
2026-01-12 18:28:30 +0530

    v

--- components/shared/TypeWriter.tsx
+++ components/shared/TypeWriter.tsx
@@ -1,13 +1,23 @@
+import { cn } from "@/lib/utils";
+
 interface TypewriterProps {
   word: string;
   count: number;
+  cursorClassName?: string;
   className?: string;
 }
 
-export function Typewriter({ word, count, className }: TypewriterProps) {
+export function Typewriter({cursorClassName, word, count, className }: TypewriterProps) {
   return (
     <span className={className}>
       {word.substring(0, count)}
+
+       <span
+        className={cn(
+          "inline-block w-[3px] ml-2 h-[1em] bg-neutral-900 animate-blink",
+          cursorClassName
+        )}
+      />
     </span>
   );
 }
