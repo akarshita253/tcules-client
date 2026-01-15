1a441fe31bf52d744679e68162eefac4776a6058
akarshita <akarshita253@gmail.com>
2026-01-15 18:36:22 +0530

    added revalidate 300 seconds to layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { HomepageQuery } from "@/lib/codegen/graphql";
 import { HOME_PAGE_QUERY } from "@/lib/queries/getHomepage";
 import { strapiRequest } from "@/lib/utils";
 import HomePageContainer from "@/features/homepage/HomePageContainer";
-export const revalidate = 30;
+
 const Homepage = async ()=> {
   const response = await strapiRequest<HomepageQuery>(HOME_PAGE_QUERY);
   const homepageData = response.homepage;
